597d3ed1f8b1633eff27c8b436d902b974e9a800
Lucas Natoli <lucas.natoli@gmail.com>
2018-08-13 15:52:51 -0300

    created at bug

--- router/dht11.js
+++ router/dht11.js
@@ -4,7 +4,7 @@ module.exports = (app, db) => {
   app.get('/dht11', (req, res) => {
     db.dht11.findAll({
       limit: 1,
-      order: [ [ 'createdAt', 'DESC' ]]
+      order: [ [ 'created_at', 'DESC' ]]
     }).then(function(dht11){
       res.json(dht11)
     })
